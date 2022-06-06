8407be201ddcdaca05e852ebaf6fc09fffc71e1e
DaveAldon <crawford@mail.gvsu.edu>
2022-06-06 02:15:34 -0400

    Added loader config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
-module.exports = nextConfig
+export default nextConfig;
