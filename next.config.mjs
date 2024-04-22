c78e1697ef968980d1364acd9c5c54b314fac1f5
David Crawford <crawford@mail.gvsu.edu>
2024-04-22 14:47:39 -0400

    Added basepath

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const nextConfig = {
   basePath: "/Next.js-and-GitHub-Pages-Example",
   output: "export",
   images: {
-    loader: "default",
+    loader: "akamai",
     path: "/Next.js-and-GitHub-Pages-Example/",
   },
-  assetPrefix: "/Next.js-and-GitHub-Pages-Example/",
+  assetPrefix: "/Next.js-and-GitHub-Pages-Example",
 };
 
 export default nextConfig;
