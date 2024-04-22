9fce484062c9b7e0f37129116732ee902be42c4a
David Crawford <crawford@mail.gvsu.edu>
2024-04-22 14:25:43 -0400

    Added basepath

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  basePath:
-    process.env.NODE_ENV === "production"
-      ? "/Next.js-and-GitHub-Pages-Example"
-      : "",
+  basePath: "/Next.js-and-GitHub-Pages-Example",
   output: "export",
   images: {
     loader: "akamai",
