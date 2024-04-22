78ccb29c101aa5389b21bdeb4d5f782b7fd4c091
David Crawford <crawford@mail.gvsu.edu>
2024-04-22 14:19:44 -0400

    Added basePath flag

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/Next.js-and-GitHub-Pages-Example"
+      : "",
   output: "export",
   images: {
     loader: "akamai",
