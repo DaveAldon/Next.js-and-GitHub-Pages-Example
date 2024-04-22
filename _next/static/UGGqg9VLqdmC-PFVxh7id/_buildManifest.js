45605d5893b90f1d7a5353f7b75798ed197d73ed
DaveAldon <DaveAldon@users.noreply.github.com>
2024-04-22 18:59:44 +0000

    Deploying to public from @ DaveAldon/Next.js-and-GitHub-Pages-Example@4c0fa37b3d886c4388efb1f1bc458d1ee8c8f67c 🚀

---
+++ _next/static/UGGqg9VLqdmC-PFVxh7id/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f7bac67f9d9badbc.css","static/chunks/pages/index-2cea165be59455d9.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
